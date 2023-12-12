b7b097900d09db5d0112e1c3db1ace14aa68859f
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-12 13:21:51 +0700

    feat: abstract class

---
+++ belajar_dart_oop/bin/abstact_class.dart
@@ -0,0 +1,8 @@
+import 'data/location.dart';
+
+void main(){
+  var city = City('Jakarta');
+  print(city.name);
+
+  //var loc = Location(); => error
+}
---
+++ belajar_dart_oop/bin/import_file.dart
@@ -0,0 +1,6 @@
+import 'data/category.dart';
+
+void main(){
+  var category = Category('GAD', 'Gadget');
+  print(category.toString());
+}
