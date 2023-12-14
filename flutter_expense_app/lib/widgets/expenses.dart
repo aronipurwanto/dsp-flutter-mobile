b59f5c4c453d8d904a35c99b5e859e23b8ee49a5
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-14 15:28:09 +0700

    feat: input text

--- flutter_expense_app/lib/widgets/expenses.dart
+++ flutter_expense_app/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_app/widgets/new_expenses.dart';
 
 import '../models/expense.dart';
 import 'expenses_list/expenses_list.dart';
@@ -28,12 +29,20 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense App'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
       ),
       body: Column(
         children: [
