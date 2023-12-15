cde40a8db63b6d1ab5af6ddab18919319f70f2cb
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-15 11:37:24 +0700

    feat: init meal app

---
+++ flutter_meal/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meal/categories.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const MealApp());
+}
+
+class MealApp extends StatelessWidget {
+  const MealApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: theme, home: const CategoriesScreen());
+  }
+}
