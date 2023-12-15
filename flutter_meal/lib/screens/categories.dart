ca051408d9d65f2f69cf5acedc2a10f092ee2b4a
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-15 15:08:11 +0700

    add message

--- flutter_meal/lib/screens/categories.dart
+++ flutter_meal/lib/screens/categories.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_meal/models/category.dart';
 
 import '../data/dummy_data.dart';
+import '../models/meal.dart';
 import '../widgets/category_grid_item.dart';
 import 'meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
@@ -18,6 +21,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
